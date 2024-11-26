216f04db871deb63b38eea0affa480f921e2e9fb
mohitM0 <mohit@m0.ventures>
2024-11-27 00:05:52 +0530

    created frame to show after transaction data

---
+++ app/frames/tx/success/route.tsx
@@ -0,0 +1,20 @@
+import { Button } from "frames.js/next";
+import { frames } from "../../frames";
+
+const handleRequest = frames(async (ctx) => {
+  return {
+    image: (
+      <div tw="flex flex-col">
+        <div tw="flex flex-col">Successfully transfered 1 USDC</div>
+      </div>
+    ),
+    buttons: [
+      <Button action="post" target={"/"}>
+        Back
+      </Button>,
+    ],
+  };
+});
+
+export const GET = handleRequest;
+export const POST = handleRequest;
