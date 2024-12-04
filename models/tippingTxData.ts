a043cad640cf246a85695de6109617f914ac030e
mohitM0 <mohit@m0.ventures>
2024-12-04 12:45:11 +0530

    modified models according to the smart contract

--- models/tippingTxData.ts
+++ models/tippingTxData.ts
@@ -2,15 +2,21 @@ import mongoose, { Document, Model, Schema } from "mongoose";
 
 interface TipDocument extends Document {
     _id: mongoose.Types.ObjectId;
+    title: string;
+    description: string;
     recipientAddress: string;
     senderAddress: string;
     amount: string;
+    isActive: boolean;
 }
 
 const TipSchema: Schema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
     recipientAddress: { type: String, required: true },
     senderAddress: { type: String, required: true },
-    amount: { type: String, required: true }
+    amount: { type: String, required: true },
+    isActive: { type: Boolean, required: true }
 }, { timestamps: true });
 
 const Tip: Model<TipDocument> = mongoose.models.Tip || mongoose.model<TipDocument>('Tip', TipSchema);
