c4eacb4f0eacb10e6336b730747e1d334e242c17
mohitM0 <mohit@m0.ventures>
2024-12-06 16:58:31 +0530

    added buttons to give tip, claim tip and post button for testing

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -3,21 +3,29 @@ import { frames } from "./frames";
 
 const handleRequest = frames(async (ctx) => {
 
-    const { tipTo } = ctx.searchParams;
+    const { tipId } = ctx.searchParams;
     
     return {
         image: (
             <div tw="bg-yellow-800 text-white w-full h-full flex flex-col justify-center items-center p-4">
                 <h2 tw="text-4xl font-bold mb-8 text-center">Tipping Frame</h2>
-                <p tw="text-3xl text-center mt-4">Tip to {tipTo}</p>
+                <p tw="text-3xl text-center mt-4">Tip Id {tipId} </p>
             </div>
         ),
         buttons:[
-            <Button action="tx" target={"/tx"} post_url={"/tx/success"}>
-                Tip
+            <Button action="tx" target={"/txGiveTip"} post_url={"/txGiveTip/success"}>
+                Send Tip
+            </Button>,
+            <Button action= "post" target={"/txGiveTip"}>
+                click me
+            </Button>,
+            <Button action="tx" target={"/txClaimTip"} post_url={"/txClaimTip/success"}>
+                Claim your Tip
             </Button>
-        ]
-
+        ],
+        textInput:(
+            "Enter Tip amount here"
+        )
     }
 })
 
